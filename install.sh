e8ed4e7d4890bd012bf7a55590772e7fbc0f8230
kntngmn <kntngmn@gmail.com>
2020-07-17 19:48:36 +0900

    add zsh/prezto setup

--- install.sh
+++ install.sh
@@ -1,11 +1,34 @@
-#!/bin/bash
+#!/bin/zsh
 
-DITFILES=(emacs.d)
+DOTFILES=(emacs.d)
+ZSHFILES=(zpreztorc zshrc)
+
+# add submodule
+git submodule update --init --recursive
+
+# for prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 
 echo "start setup..."
-for fname in ${DITFILES[@]};
+for dotfile in ${DOTFILES[@]};
 do
-    ln -svi $PWD/$fname $HOME/.$fname
+    ln -svf $PWD/$dotfile $HOME/.$dotfile
 done
+
+echo "start zsh setup..."
+for zshfile in ${ZSHFILES[@]};
+do
+    ln -svf $PWD/$zshfile $HOME/.zprezto/runcoms/$zshfile
+done
+
+# change shell to zsh
+chsh -s $(which zsh)
+
+source ~/.zshrc
+source ~/.zpreztorc
 
 echo "complete!"
